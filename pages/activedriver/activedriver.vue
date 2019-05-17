324372f7c98869d856d59a7f8193c47f10978015
love1234 <345841634@qq.com>
2019-05-17 16:06:02 +0800

    first commit

---
+++ pages/activedriver/activedriver.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="main">
+		<view class="input-group">
+			<view class="input-row border">
+				<m-input class="m-input" type="text" clearable focus v-model="account" placeholder="请输入身份证后六位"></m-input>
+			</view>
+			<view class="phone">
+				手机号码: 1320168837
+			</view>
+			<view class="verify-code">
+				<m-input class="code_input" type="number" focus clearable v-model="code" placeholder="请输入验证码"></m-input>
+				<button type="primary" class="primary" @tap="register">获取验证码</button>
+			</view>
+		</view>
+		<view class="btn-row">
+			<button type="primary" class="primary" @tap="register">激活</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	import mInput from '@/components/m-input.vue';
+
+	export default {
+		components: {
+			mInput
+		},
+		data() {
+			return {
+				account: '',
+				code: ''
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.main {
+		width: 100%;
+		padding: 0 10%;
+	}
+
+	.input-group {
+		/* width: 100%; */
+		border: none;
+	}
+
+	.input-row {
+		border: solid 1upx #C8C7CC;
+		border-radius: 8upx;
+		height: 80upx;
+		line-height: 80upx;
+		margin-top: 20upx;
+		/* width: 80%; */
+		margin: auto;
+	}
+
+	.phone {
+		height: 60upx;
+		line-height: 60upx;
+		color: #8F8F94;
+		font-size: 24upx;
+		/* text-align: center; */
+	}
+
+	.verify-code {
+		/* width: 80%; */
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		/* padding: 0 15upx; */
+		box-sizing: border-box;
+
+		margin-top: 20upx;
+	}
+
+	.code_input {
+		border: solid 1upx #C8C7CC;
+		height: 80upx;
+		line-height: 80upx;
+		margin-right: 10upx;
+	}
+</style>
---
+++ pages/roleselect/roleselect.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="main">
+		<image class="logo" src="http://qnimage.xiteng.com/1.jpg" ></image>
+		
+		<view class="item">
+			司机
+		</view>
+		<view class="item">
+			经销商
+		</view>
+		<view class="item">
+			中心工厂
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.main{
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	
+	.logo{
+		width: 180upx;
+		height: 180upx;
+		margin-top: 60upx;
+		margin-bottom: 80upx;
+	}
+	
+	.item{
+		border: solid 1upx #C8C7CC;
+		border-radius: 8upx;
+		height: 80upx;
+		line-height: 80upx;
+		text-align: center;
+		margin-top: 20upx;
+		width: 80%;
+	}
+
+</style>
